6abdaa5e9fd54e15ddf2be29182be37e2013d88b
Aiman Errajiy <aerrajiy@e1r3p5.1337.ma>
2022-12-23 21:43:43 +0100

    push mandatory part with sad face again :(

---
+++ tools/pipex_tools.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_tools.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerrajiy <aerrajiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 19:11:34 by aerrajiy          #+#    #+#             */
+/*   Updated: 2022/12/23 21:38:03 by aerrajiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/pipex.h"
+
+char	**read_and_split_path(char *env[])
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strstr(env[i], "PATH"))
+			return (ft_split(env[i] + 5, ':'));
+		i++;
+	}
+	return (NULL);
+}
+
+void	check_path_error(char *path)
+{
+	if (access(path, F_OK) == -1)
+	{
+		write(2, "pip : command not found\n", 25);
+		exit(127);
+	}
+}
